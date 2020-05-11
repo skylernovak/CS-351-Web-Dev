e5fd9d3cdf2e4217c73d40669f79d783034da739
Skyler Novak <skylernovak@yahoo.com>
2020-05-11 10:29:00 -0700

    Q2 part C done

--- Q2D/SOLLanguage.js
+++ Q2D/SOLLanguage.js
@@ -1,9 +1,43 @@
 import React from 'react';
 
-function Language(props) {
-    
-    return <div>
-    </div>;
-}
+class Language extends React.Component {
+    constructor(props) {
+      super(props); // Must call
+      this.state = {
+          name: "", info: ""
+      };
+    } // constructor
 
-export default Language;
+    textAreachange(event){
+        this.setState({info: event.currentTarget.value});
+    }
+  
+    inputChange(event){
+        this.setState({name: event.currentTarget.value});
+    }
+
+    addLang() {
+        console.log(this.state);
+    }
+
+    render() {
+        let formTitle = <h2>Language Form</h2>;
+        let contents = <div id="LanguageForm">
+            <label htmlFor="langName">Name: </label>
+            <input id="langName" value={this.state.name} onChange={this.inputChange.bind(this)}/>
+            <label htmlFor="langInfo">Tell us about the language: </label>
+            <textarea id="langInfo" value={this.state.info} onChange={this.textAreachange.bind(this)}/>
+            <button type="submit" onClick={this.addLang.bind(this)}>Send Language</button>
+        </div>;
+
+      return <div>
+          {formTitle}
+          {contents}
+      </div>;
+
+    } // return()
+  
+} // Language class component
+  
+  
+  export default Language;
